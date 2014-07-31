3d19742e4fb25954e02a6c960f7aa5a61a955bab
Christian Harrington <christian@harrington.dk>
2014-07-31 17:52:14 +0200

    Add stars again.

--- YASG/Assets/Scripts/Behaviour/UniverseGenerator.cs
+++ YASG/Assets/Scripts/Behaviour/UniverseGenerator.cs
@@ -14,8 +14,9 @@ public class UniverseGenerator : MonoBehaviour {
 		foreach (Vector2 p in starPositions) {
 			GameObject s = Instantiate (StarSystem, new Vector3 (p.x, 0, p.y), Quaternion.identity) as GameObject;
 
-//			s.GetComponent<StarSystem>().GameState = GameState;
-//			GameState.Turnables.Add(s.GetComponent<StarSystem>());
+			StarBehaviour sb = s.GetComponent<StarBehaviour>();
+			sb.GameState = GameState;
+			GameState.Turnables.Add(sb.Star);
 
 			float r = UnityEngine.Random.Range(0.01f, 0.1f);
 			Vector3 scale = new Vector3(r, r, r);
