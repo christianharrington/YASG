aae525f8dbe7bf619a5d40d184e081b77efa8b3f
Christian Harrington <christian@harrington.dk>
2014-07-31 16:10:03 +0200

    Added planets to Constants.

--- YASG/Assets/Scripts/Constants.cs
+++ YASG/Assets/Scripts/Constants.cs
@@ -17,23 +17,44 @@ public class Constants {
 
 	public static Dictionary<StarType, StellarObjectData> StarData = new Dictionary<StarType, StellarObjectData> ()
 	{
-		{ StarType.O, new StellarObjectData(6.6, 100, 1) },
-		{ StarType.B, new StellarObjectData(1.8, 6.6, 1) },
-		{ StarType.A, new StellarObjectData(1.4, 1.8, 1) },
-		{ StarType.F, new StellarObjectData(1.15, 1.4, 1) },
-		{ StarType.G, new StellarObjectData(0.96, 1.15, 1) },
-		{ StarType.K, new StellarObjectData(0.7, 0.96, 1) },
-		{ StarType.M, new StellarObjectData(0, 0.7, 1) } 
+		{ StarType.O, new StellarObjectData(6.6, 100, 1, 1) },
+		{ StarType.B, new StellarObjectData(1.8, 6.6, 1, 1) },
+		{ StarType.A, new StellarObjectData(1.4, 1.8, 1, 1) },
+		{ StarType.F, new StellarObjectData(1.15, 1.4, 1, 1) },
+		{ StarType.G, new StellarObjectData(0.96, 1.15, 1, 1) },
+		{ StarType.K, new StellarObjectData(0.7, 0.96, 1, 1) },
+		{ StarType.M, new StellarObjectData(0, 0.7, 1, 1) } 
 	};
 	#endregion
+
+	#region Planet Data
+	// http://www.nasa.gov/content/nasa-kepler-results-usher-in-a-new-era-of-astronomy/
+	public static Dictionary<PlanetType, Double> PlanetTypeLikelyhood = new Dictionary<PlanetType, Double>() {
+		{ PlanetType.EarthLike, 0.190503 },
+		{ PlanetType.SuperEarth, 0.304126 },
+		{ PlanetType.NeptuneLike, 0.411814 },
+		{ PlanetType.JupiterLike, 0.068195 },
+		{ PlanetType.SuperJupiter, 0.028829 }
+	};
+
+	public static Dictionary<StarType, StellarObjectData> PlanetData = new Dictionary<PlanetType, StellarObjectData> ()
+	{
+		{ PlanetType.EarthLike, new StellarObjectData(0.3, 1.25, 0.06, 1.25) },
+		{ PlanetType.SuperEarth, new StellarObjectData(1.25, 2, 1.00, 1.5) },
+		{ PlanetType.NeptuneLike, new StellarObjectData(2, 6, 2, 18) },
+		{ PlanetType.JupiterLike, new StellarObjectData(6, 15, 18, 320) },
+		{ PlanetType.SuperJupiter, new StellarObjectData(15, 7, 320, 400) }
+	};
+	#endregion Planet Data
 }
 
 public struct StellarObjectData {
-	public readonly Double MinRadius, MaxRadius, Density;
+	public readonly Double MinRadius, MaxRadius, MinMass, MaxMass;
 
-	public StellarObjectData(Double min, Double max, Double dens) {
-		MinRadius = min;
-		MaxRadius = max;
-		Density = dens;
+	public StellarObjectData(Double minR, Double maxR, Double minM, Double maxM) {
+		MinRadius = minR;
+		MaxRadius = maxR;
+		MinMass = minM;
+		MaxMass = maxM;
 	}
 }
