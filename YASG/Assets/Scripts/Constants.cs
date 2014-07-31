3de37ce982bed6e1da32b1be21d5e2aa5bc44bdb
Sune Alkærsig <sual@itu.dk>
2014-07-31 15:25:09 +0200

    Changed StellarObjectData struct.

--- YASG/Assets/Scripts/Constants.cs
+++ YASG/Assets/Scripts/Constants.cs
@@ -14,11 +14,26 @@ public class Constants {
 		{ StarType.K, 0.121 },
 		{ StarType.M, 0.7645 }
 	};
+
+	public static Dictionary<StarType, StellarObjectData> StarData = new Dictionary<StarType, StellarObjectData> ()
+	{
+		{ StarType.O, new StellarObjectData(6.6, 100, 1) },
+		{ StarType.B, new StellarObjectData(1.8, 6.6, 1) },
+		{ StarType.A, new StellarObjectData(1.4, 1.8, 1) },
+		{ StarType.F, new StellarObjectData(1.15, 1.4, 1) },
+		{ StarType.G, new StellarObjectData(0.96, 1.15, 1) },
+		{ StarType.K, new StellarObjectData(0.7, 0.96, 1) },
+		{ StarType.M, new StellarObjectData(0, 0.7, 1) } 
+	};
 	#endregion
 }
 
 public struct StellarObjectData {
-	public Double MaxRadius
-	public Double MinRadius
-	public Double Density
+	public readonly Double MinRadius, MaxRadius, Density;
+
+	public StellarObjectData(Double min, Double max, Double dens) {
+		MinRadius = min;
+		MaxRadius = max;
+		Density = dens;
+	}
 }
